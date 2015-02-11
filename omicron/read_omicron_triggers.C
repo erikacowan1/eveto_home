acf805ce9f05c4dc7fa74bdf1c7ef43731957afe
Duncan Brown <duncan.brown@ligo.org>
2015-02-11 13:51:18 -0500

    fixed code so that channel names are printed from tree in loop

--- omicron/read_omicron_triggers.C
+++ omicron/read_omicron_triggers.C
@@ -26,21 +26,19 @@ int read_omicron_triggers( TString trigger_directory, TString safe_channel_file 
   Long64_t num_veto_channels = safe_channel_tree->ReadFile( safe_channel_file, "channel/C" );
   std::cout << "Read " << num_veto_channels << " channel names" << std::endl;
 
+  safe_channel_tree->Print();
+
   std::cout << "Reading triggers from directory " << trigger_directory << std::endl;
   Long64_t num_safe_channels = safe_channel_tree->GetEntries();
   TTree* veto_trigger_tree_array[num_safe_channels];
  
-//  std::string *channel_name = 0;
-//  safe_channel_tree->SetBranchAddress("channel", &channel_name);
+  char *channel_name = new char[256];
+  safe_channel_tree->SetBranchAddress("channel", channel_name);
 
   std::cout << "number of channels = " << num_safe_channels << std::endl;
-
   for( Long64_t i = 0; i < num_safe_channels; i++ )
   {
    TTree * new_tree = (TTree*)f->Get("channel");
-   std::string * channel_name = new std::string(); 
-   // std::string *channel_name = 0;
-    safe_channel_tree->SetBranchAddress("channel", &channel_name); 
     std::cout << "i = " << i << std::endl;
     safe_channel_tree->GetEntry(i);
     std::cout << "Reading channel " << channel_name <<  std::endl;
