87a96f4aeb9895d5d36b6abb84fc59b04bbd412b
Duncan Brown <duncan.brown@ligo.org>
2015-02-13 15:12:00 -0500

    code seems to read in the omicron triggers successfully

--- omicron/read_omicron_triggers.C
+++ omicron/read_omicron_triggers.C
@@ -15,6 +15,21 @@
 #include <THStack.h>
 #include <TBrowser.h>
 #include <TFile.h>
+#include <TChain.h>
+
+TChain* read_omicron_channel( TString trigger_directory, const char* channel_name ) {
+
+  std::cout << "Creating triggers chain for " << channel_name << std::endl;
+  TChain* veto_trigger_chain = new TChain( "triggers", channel_name );
+
+  char *veto_file_pattern = new char[512];
+  snprintf( veto_file_pattern, 512, "%s/%s/*.root", trigger_directory.Data(), channel_name );
+
+  std::cout << "Adding files " << veto_file_pattern << std::endl;
+  veto_trigger_chain->Add(veto_file_pattern);
+
+  return veto_trigger_chain;
+}
 
 int read_omicron_triggers( TString trigger_directory, TString safe_channel_file ) {
 
@@ -30,7 +45,7 @@ int read_omicron_triggers( TString trigger_directory, TString safe_channel_file 
 
   std::cout << "Reading triggers from directory " << trigger_directory << std::endl;
   Long64_t num_safe_channels = safe_channel_tree->GetEntries();
-  TTree* veto_trigger_tree_array[num_safe_channels];
+  TChain* veto_trigger_tree_array[num_safe_channels];
  
   char *channel_name = new char[256];
   safe_channel_tree->SetBranchAddress("channel", channel_name);
@@ -38,21 +53,29 @@ int read_omicron_triggers( TString trigger_directory, TString safe_channel_file 
   std::cout << "number of channels = " << num_safe_channels << std::endl;
   for( Long64_t i = 0; i < num_safe_channels; i++ )
   {
-   TTree * new_tree = (TTree*)f->Get("channel");
     std::cout << "i = " << i << std::endl;
     safe_channel_tree->GetEntry(i);
     std::cout << "Reading channel " << channel_name <<  std::endl;
-    veto_trigger_tree_array[i] = NULL;
+    veto_trigger_tree_array[i] = read_omicron_channel( trigger_directory, channel_name );
+    veto_trigger_tree_array[i]->Print();
   }
 
   new TBrowser;
   return 0;
 }
 
+
 int main( int argc, char *argv[] )
 {
-  TApplication* rootapp = new TApplication("read_omicron_triggers",&argc, argv);
-  read_omicron_triggers( argv[1], argv[2] );
+  char path[512];
+  char safe_file[512];
+
+  fprintf( stderr, "%s %s %s\n", argv[0], argv[1], argv[2] );
+  snprintf( path, 512, argv[1] );
+  snprintf( safe_file, 512, argv[2] );
+
+  TApplication* rootapp = new TApplication("read_omicron_triggers", &argc, argv);
+  read_omicron_triggers( path, safe_file );
   rootapp->Run();
   return 0;
 }
