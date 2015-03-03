5bf18e53fc0c5f3eb1d45f1e1acab04cd75ce3df
Duncan Brown <duncan.brown@ligo.org>
2015-03-03 14:06:04 -0500

    set command line options to read cbc triggers

--- test_cbc_eveto.sh
+++ test_cbc_eveto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -v
 
 ./cbc_eveto \
-    --gps-start-time 9000000000 \
-    --gps-end-time   9000000000 \
+    --gps-start-time 1096275690 \
+    --gps-end-time 1096290950 \
     --detector L1 \
     --safe-channel-file safe_channel_list.txt \
-    --cbc-trigger-database /path/to/cbc \
+    --cbc-trigger-database /Users/dbrown/Documents/Research/detchar/eveto/l1-lock-clustered.sqlite \
     --cbc-snr-threshold 5.0 \
     --omicron-trigger-path /Users/dbrown/Documents/Research/detchar/triggers \
     --omicron-snr-threshold 8.0 \
