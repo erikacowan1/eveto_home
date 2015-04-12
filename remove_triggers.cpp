2aa486e294d97dc6e03d551d4405a171306cfb36
Erika Cowan <erika.cowan@ligo.org>
2015-04-12 16:46:36 -0400

    made template for remove_triggers algorithm.

---
+++ remove_triggers.cpp
@@ -0,0 +1,18 @@
+#include "cbc_eveto.h"
+
+//code to remove veteod triggers from cbc trigs//
+
+//function that does a simple significance calculation
+int eveto::calc_dumb_sig(
+    TTree* cbc_trigger_tree_ptr,
+    TTree* omicron_trigger_tree_ptr,
+    bool verbose )
+    {
+    
+
+
+
+
+
+
+    }            
