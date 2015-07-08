edd1438c37c2963b931ece824f0f5423a2201e9b
Erika Cowan <erika.cowan@ligo.org>
2015-07-08 18:41:10 -0400

    added sqlite database path for CBC triggers

--- test_cbc_eveto.sh
+++ test_cbc_eveto.sh
@@ -5,13 +5,12 @@
     --gps-end-time 1096290950 \
     --detector L1 \
     --safe-channel-file safe_channels.txt \
-    --cbc-trigger-database l1-lock-clustered.sqlite \
+    --cbc-trigger-database /home/erika.cowan/data/triggers/ER7/CBC/L1-16SEC_CLUSTERED_CBC_ER7_FULL_DATA.sqlite \
     --cbc-snr-threshold 5.0 \
     --omicron-trigger-path /home/dbrown/public_html/detchar/omicron/L1/triggers/  \
     --omicron-snr-threshold 8.0 \
     --omicron-cluster-window 1.0 \
-    --omicron-start-time 1096275690 \
-    --omicron-end-time 1096290950 \
+    --cwb-trigger-path /home/erika.cowan/data/triggers/ER7/CWB/ \
     --output-directory . \
     --sig-threshold 5.0 \
     --dumb-veto-window 3.0 \
