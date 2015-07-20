0686d9dbc1fd5b83702747ae48a160d386c57f5e
Erika Cowan <erika.cowan@ligo.org>
2015-07-20 02:39:34 -0400

    compiles! uses a TChain to bring together all of the root files. Still need to make sure it reads in  the right branches later on in the code

--- cbc_eveto_read_CWB.cpp
+++ cbc_eveto_read_CWB.cpp
@@ -3,20 +3,21 @@
 //Function that chains .root files together for CWB triggers
 
 int eveto::read_cwb_triggers(
-	TTree* cwb_tchain_tree,
+	TChain* cwb_tchain_tree,
 	TString* cwb_trigger_path,
 	bool verbose )
 {
+	Int_t retcode;
 	if ( verbose ) std::cout << "reading CWB triggers for" << cwb_trigger_path << std::endl;
 
+	char *veto_file_pattern = new char[512];
 	snprintf( veto_file_pattern, 512, "%s/wave_111*.root", cwb_trigger_path->Data());
 
-	TChain* cwb_tchain_tree = new TChain("cwb_tchain_tree");
 	retcode = cwb_tchain_tree->Add(veto_file_pattern);
 
 	if ( retcode <= 0 ) {
     	std::cerr << "could not read any triggers from " << veto_file_pattern << std::endl;
         return false;
 	}
-
+	return 0;
 }
