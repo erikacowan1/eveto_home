68816bb04dfee2bd0403c396852d41a02663dc45
erikacowan1 <erikacowan1@gmail.com>
2015-07-21 12:45:34 -0400

    added in temporary veto code for CWB. Will merge with CBC after I get this part working

--- cbc_eveto_main.cpp
+++ cbc_eveto_main.cpp
@@ -77,7 +77,7 @@ int eveto::cbc_eveto_main(
 	// Read in the CWB triggers for the interval that we want to process
 	//
 	retcode = eveto::read_cwb_triggers(
-		cwb_tchain_tree,
+		&cwb_tchain_tree,
 		cwb_trigger_path,
 		verbose ) ;
 
@@ -113,10 +113,151 @@ int eveto::cbc_eveto_main(
 		}
 	}
 	
-	if( !(detector == &cbc) && !(detector == &cwb)) {
+/*	if( !(detector == &cbc) && !(detector == &cwb)) {
 		std::cerr << "channel other than cbc/cwb has been called. " << std::endl;
 		return 0;
 	}
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+	//TEMP CWB Veto Algorithm
+	//currently the segments are stored in omicron_triggers.root/segments. For now
+	//assume that all segments are science segments. Grab segments from segment tree
+	//and perform veto algorithm. 
+
+	//int num_safe_channels = safe_channels->GetEntries();
+	float max_sig = sig_threshold;
+	int i; //counts over safe channels
+	float sig[num_safe_channels]; //array to store significance of each channel
+	int max_sig_index = -1; // index of the winning channel!
+	int r = 1; //round counter
+
+
+	//define TTrees
+	TTree* cwb_trigs_round[max_rounds + 1];
+	TTree* omicron_trigs_round[max_rounds + 1][num_safe_channels];
+	//TTree* cbc_segs_round[max_rounds + 1][num_safe_channels]; //doesn't currently exist
+	//TTree* omicron_segs_round[max_rounds + 1][num_safe_channels];
+
+
+	//initialize arrays
+	cwb_trigs_round[0] = cwb_tchain_tree;
+	std::cerr << "Initialized CWB treel for round 0: " << cwb_trigs_round[0] << std::endl;
+	//cbc_segs_round[0] = cbc_segs_tree;
+
+	std::cerr << "Number of safe channels = " << num_safe_channels << std::endl;
+	for (i=0; i<num_safe_channels; ++i) {
+		omicron_trigs_round[0][i] = clustered_veto_trigger_tree[i]; //check name
+		if (verbose) std::cout << "Initialized veto channel for round 0: " << omicron_trigs_round[0][i]->GetName() << std::endl;
+	}
+
+
+	// Begin the loop over rounds and break if all channels go below the max
+	// significance threshold or we exceed the maximum number of rounds
+	//float max_sig = sig_threshold;
+	//int r = 1; //round counter
+	while (max_sig >= sig_threshold && r <= max_rounds ) {
+		if (verbose) std::cerr << "==== Processing round " << r << " of " << max_rounds << " ====" << std::endl;
+		max_sig_index = -1;
+
+		// loop over veto channels, caculate significance of channel,
+		// find the channel with the highest significance, and store index
+		for (i=0; i<num_safe_channels; ++i) {
+
+			if (omicron_trigs_round[r-1][i] != NULL) {
+
+				if ( verbose ) std::cerr << "calculating dumb significance for veto tree " << omicron_trigs_round[r-1][i]->GetName() << "(" << omicron_trigs_round[r-1][i] << ") against cbc triggers (" << cwb_trigs_round[r-1] << ")" << std::endl;
+
+				sig[i] = eveto::calc_dumb_sig(cwb_trigs_round[r-1], omicron_trigs_round[r-1][i], dumb_veto_window, verbose);
+				if ( verbose ) std::cerr << "Significance for " << omicron_trigs_round[r-1][i]->GetName() << " = " << sig[i] << std::endl;
+			} else {
+				sig[i] = 0;
+			}
+
+			if(sig[i] >= max_sig) {
+				max_sig = sig[i];
+				max_sig_index = i;
+			}
+		}
+
+		if ( max_sig_index == -1 )
+		{
+			std::cerr << "There are no winners!" << std::endl;
+			break;
+		}
+
+		if ( verbose ) std::cerr << "Winning channel was " << omicron_trigs_round[r-1][max_sig_index]->GetName() << std::endl;
+
+		cwb_trigs_round[r] = eveto::remove_cwb_triggers(cwb_trigs_round[r-1], omicron_trigs_round[r-1][max_sig_index], verbose);
+
+		for (i=0; i<num_safe_channels; ++i) {
+			if ( (i != max_sig_index) && (omicron_trigs_round[r-1][i] != NULL) ) {
+				omicron_trigs_round[r][i] = eveto::remove_omicron_triggers(omicron_trigs_round[r-1][i], omicron_trigs_round[r-1][max_sig_index], verbose);
+			}
+			else {
+				omicron_trigs_round[r][i] = NULL;
+			}
+		}
+
+#if 0
+		cbc_trigs_round[r] = remove_triggers(cbc_trigs_round[r-1], omicron_trigs_round[r-1][max_sig_index],cbc_segs_round[r-1], omicron_segs_round[r-1][max_sig_index]);
+
+		if (i != max_sig_index) {	
+			omicron_trigs_round[r][i] = remove_triggers(omicron_trig_round[r-1][i], omicron_trigs_round[r-1][max_sig_index], omicron_segs_round[r-1][i], omicron_segs_round[r-1][max_sig_index]); //should segments have their own remove triggers function?
+
+		}
+
+		else{
+			i = NULL;
+		}
+#endif
+
+		if (verbose) std::cerr << "Finished round " << r << std::endl;
+		if (verbose) std::cerr << "Maximum significance was " << max_sig << std::endl;
+		r += 1;
+	
+	/*
+	for (i=0; i<num_safe_channels; ++i) {
+		safe_channels->GetEntry(i);
+		char *name_safe_channel_tree = new char[256];
+		snprintf(name_safe_channel_tree, 256, "VETOTRIGS:%s", safe_channels);
+
+		retcode = eveto::cbc_eveto_analysis( cbc_trigs_round[r], omicron_trigs_round[r][i], verbose); //want the newly vetoed trigger trees for cbc and omicron
+
+		if ( retcode ) {
+			std::cerr << "error plotting new cbc triggers, in cbc_eveto_analysis" << std::endl;
+			return 1;
+		}
+	}
+
+*/
+	}
+        std::cerr << "Eveto is finished" << std::endl;
+        return 0;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
 
 	//Veto Algorithm
 	//currently the segments are stored in omicron_triggers.root/segments. For now
@@ -214,7 +355,7 @@ int eveto::cbc_eveto_main(
 		if (verbose) std::cerr << "Maximum significance was " << max_sig << std::endl;
 		r += 1;
 	
-	/*
+	////start commented out////
 	for (i=0; i<num_safe_channels; ++i) {
 		safe_channels->GetEntry(i);
 		char *name_safe_channel_tree = new char[256];
@@ -227,9 +368,12 @@ int eveto::cbc_eveto_main(
 			return 1;
 		}
 	}
+	////end commented out////
 
-*/
+
 	}
         std::cerr << "Eveto is finished" << std::endl;
         return 0;
+*/
+
 }
