84e93a6f33cc9c43581e186ca534fafe697424eb
Erika Cowan <erika.cowan@ligo.org>
2015-08-19 01:32:06 -0400

    committing, pushing, and then checking parenthesis on gvim. If all goes well, it should compile

--- cbc_eveto_significance.cpp
+++ cbc_eveto_significance.cpp
@@ -1,3 +1,7 @@
+#include "cbc_eveto.h"
+#include "TLeaf.h"
+//#include <boost/math/special_functions/gamma.hpp>
+
 //
 //
 //Code to calculate real significance
@@ -6,7 +10,7 @@
 //
 
 
-int cbc_eveto_significance(
+int eveto:: cbc_eveto_significance(
 	int sig_threshold,
 	TTree* main_channel_trigger_tree_ptr,
 	TTree* omicron_trigger_tree_ptr,
@@ -16,7 +20,7 @@ int cbc_eveto_significance(
 {
 
         if( verbose ) {
-		std::cerr << "cbc_eveto_significance() got " << main_channel->Data() " tree at " << main_channel_trigger_tree_ptr << std::endl;
+		std::cerr << "cbc_eveto_significance() got " << main_channel->Data() <<" tree at " << main_channel_trigger_tree_ptr << std::endl;
 		std::cerr << "cbc_eveto_significance() got omicron tree at" << omicron_trigger_tree_ptr << std::endl;
 	}
 
@@ -38,6 +42,8 @@ int cbc_eveto_significance(
         int num_omicron_triggers = omicron_trigger_tree_ptr->GetEntries();
 	float time_window_2 = time_window/2;
         int num_coinc_triggers = 0;
+
+        const char *main_channel_char = (const char*)main_channel;
         if( strcmp(main_channel_char, "cbc") == 0)
         {
                 Double_t start_time, end_time;
@@ -58,68 +64,161 @@ int cbc_eveto_significance(
                 main_channel_trigger_tree_ptr->SetBranchAddress("ttotal",&ttotal);
 
                 int num_main_channel_triggers = main_channel_trigger_tree_ptr->GetEntries();
-		Long64_t aux_end_time, aux_start_time, main_channel_start_time, main_channel_end_time;
+	//	Double_t aux_end_time, aux_start_time, main_channel_start_time, main_channel_end_time;
 		if( num_omicron_triggers == 0 ) {
                         if( verbose ) std::cout << "no omicron triggers, returning zero significance" << std::endl;
                        return 0;
                 }
 
+
                 for( Int_t c = 0; c<num_main_channel_triggers; ++c ) {
                         main_channel_trigger_tree_ptr->GetEntry(c);
-
-			if( c = 0 ) {
-				main_channel_start_time = start_time;
+			/*TLeaf *start_time = main_channel_trigger_tree_ptr->GetLeaf("start_time");
+			if( c == 0 ) {
+				start_time->GetBranch()->GetEntry(c);
+				main_channel_start_time = start_time->GetValue();
 				break;
 			}
 			
-			if( c = num_main_channel_triggers ) {
-				main_channel_end_time = start_time;
+			if( c == num_main_channel_triggers ) {
+				start_time->GetBranch()->GetEntry(c);
+				main_channel_end_time = start_time->GetValue();
 				break;
 			}
-
+			*/
                         for(Int_t o=0; o<num_omicron_triggers; ++o) {
                                 omicron_trigger_tree_ptr->GetEntry(o);
-					
-				if( o = 0 ) {
-					aux_start_time = Ltime;
+			/*	//TLeaf *Ltime = main_channel_trigger_tree_ptr->GetLeaf("Ltime");	
+				if( o == 0 ) {
+					TLeaf *Ltime = main_channel_trigger_tree_ptr->GetLeaf("Ltime");
+					Ltime->GetBranch()->GetEntry(o);
+					aux_start_time = Ltime->GetValue();
 					break;
 				}
 
-				if( o=num_main_channel_triggers) {
-					aux_end_time=Ltime;
+				if( o == num_main_channel_triggers) {
+					TLeaf *Ltime = main_channel_trigger_tree_ptr->GetLeaf("Ltime");
+					Ltime->GetBranch()->GetEntry(o);
+					aux_end_time=Ltime->GetValue();
 					break; 
 				}
-
+			*/
 				if( start_time < (Ltime + time_window_2) && start_time > Ltime - time_window_2 ) {
 					num_coinc_triggers++;
 				}
 			}
 		}
+	////////////////////////////////////////////////
+	///////////// CWB OPTION   /////////////////////
+	////////////////////////////////////////////////
+        if( strcmp(main_channel_char, "cwb") == 0)
+        {
+                Double_t Ctime, Cfrequency, Cduration, Cstart, Cstop, Csnr, Crho, Cnetcc, Cneted, Clikelihood, Cecor, CECOR, Cifo, Crate, Cphi, Ctheta, Cpsi, Cnull, Chrss, Cnoise, Clow, Chigh, Cbandwidth, Cstrain, Crun;
+                Long64_t Csize;
 
-		//calculate significance
-		float mu;
-		Long64_t = sig, sigold; 
+                main_channel_trigger_tree_ptr->SetBranchAddress("time",&Ctime);
+                main_channel_trigger_tree_ptr->SetBranchAddress("frequency",&Cfrequency);
+                main_channel_trigger_tree_ptr->SetBranchAddress("duration",&Cduration);
+                main_channel_trigger_tree_ptr->SetBranchAddress("start",&Cstart);
+                main_channel_trigger_tree_ptr->SetBranchAddress("stop",&Cstop);
+                main_channel_trigger_tree_ptr->SetBranchAddress("snr",&Csnr);
+                main_channel_trigger_tree_ptr->SetBranchAddress("rho",&Crho);
+                main_channel_trigger_tree_ptr->SetBranchAddress("netcc",&Cnetcc);
+                main_channel_trigger_tree_ptr->SetBranchAddress("neted", &Cneted);
+                main_channel_trigger_tree_ptr->SetBranchAddress("likelihood", &Clikelihood);
+                main_channel_trigger_tree_ptr->SetBranchAddress("ecor", &Cecor);
+                main_channel_trigger_tree_ptr->SetBranchAddress("ECOR", &CECOR);
+                main_channel_trigger_tree_ptr->SetBranchAddress("ifo", &Cifo);
+                main_channel_trigger_tree_ptr->SetBranchAddress("rate", &Crate);
+                main_channel_trigger_tree_ptr->SetBranchAddress("phi", &Cphi);
+                main_channel_trigger_tree_ptr->SetBranchAddress("theta", &Ctheta);
+                main_channel_trigger_tree_ptr->SetBranchAddress("psi", &Cpsi);
+                main_channel_trigger_tree_ptr->SetBranchAddress("null", &Cnull);
+                main_channel_trigger_tree_ptr->SetBranchAddress("hrss", &Chrss);
+                main_channel_trigger_tree_ptr->SetBranchAddress("noise", &Cnoise);
+                main_channel_trigger_tree_ptr->SetBranchAddress("low", &Clow);
+                main_channel_trigger_tree_ptr->SetBranchAddress("high", &Chigh);
+                main_channel_trigger_tree_ptr->SetBranchAddress("bandwidth", &Cbandwidth);
+                main_channel_trigger_tree_ptr->SetBranchAddress("strain", &Cstrain);
+                main_channel_trigger_tree_ptr->SetBranchAddress("run", &Crun);
+                main_channel_trigger_tree_ptr->SetBranchAddress("size", &Csize);
 
-		//calculating total time analyzed: to be changed later. 
-        	Long65_t analyze_start, analyze_end, T_tot; //total time analyzed
-		
-		analyze_start = aux_start_time - time_window_2; //leftover code, not necessary
-		analyze_end = aux_end_time + time_window_2;
+                int num_main_channel_triggers = main_channel_trigger_tree_ptr->GetEntries();
 
-		T_tot = num_omicron_triggers*time_window; //total time analyzed
+                if( num_omicron_triggers == 0 ) {
+                        if( verbose ) std::cout << "no omicron triggers, returning zero significance" << std::endl;
+                        return 0;
+                }
 
-		mu = (time_window*num_main_channel_triggers*num_omicron_triggers)/T_tot;
-		
-		if( num_coinc_triggers > 0 ) {
-			sig = gamma_p(mu, num_coinc_triggers);
-			sigold = -num_coinc_triggers*log10(mu) + mu*log10(exp(1)) + lgammal(num_coinc_triggers+1)/log(10);
-			return sig;
-			std::cerr << "sig = " << sig << ". If close enough to infinity, print out sigold instead, which yields " << sigold << ". \n" << std::endl;
-		}
-		
-		else { 
-			sig = 0;
-			std::cerr << "No coincident triggers, and therefore no significance. Something is wrong." << std::endl;
-		}
 
+
+	
+		//Long64_t aux_end_time, aux_start_time, main_channel_start_time, main_channel_end_time;
+                if( num_omicron_triggers == 0 ) {
+                        if( verbose ) std::cout << "no omicron triggers, returning zero significance" << std::endl;
+                       return 0;
+                }
+
+
+                for( Int_t c = 0; c<num_main_channel_triggers; ++c ) {
+                        main_channel_trigger_tree_ptr->GetEntry(c);
+		/*	TLeaf *start_time = main_channel_trigger_tree_ptr->GetLeaf("start_time");
+                        if( c == 0 ) {
+				start_time->GetBranch()->GetEntry(c);
+                                main_channel_start_time = start_time->GetValue();
+                                break;
+                        }
+
+                        if( c == num_main_channel_triggers ) {
+                                start_time->GetBranch()->GetEntry(c);
+				main_channel_end_time = start_time->GetValue();
+                                break;
+                        }
+*/
+                        for(Int_t o=0; o<num_omicron_triggers; ++o) {
+                                omicron_trigger_tree_ptr->GetEntry(o);
+/*				TLeaf *Ltime = main_channel_trigger_tree_ptr->GetLeaf("Ltime");
+                                if( o == 0 ) {
+					Ltime->GetBranch()->GetEntry(c);
+                                        aux_start_time = Ltime->GetValue();
+                                        break;
+                                }
+
+                                if( o == num_main_channel_triggers) {
+					Ltime->GetBranch()->GetEntry(c);
+                                        aux_end_time=Ltime->GetValue();
+                                        break;
+                                }
+*/
+                                if( Cstart < (Ltime + time_window_2) && Cstart > Ltime - time_window_2 ) {
+                                        num_coinc_triggers++;
+                                }
+                        }
+                }
+	}
+
+		Float_t mu;
+                Long64_t sig, sigold;
+
+                //calculating total time analyzed: to be changed later. 
+                //Long64_t analyze_start, analyze_end, T_tot; //total time analyzed
+                //analyze_start = aux_start_time - time_window_2; //leftover code, not necessary
+                //analyze_end = aux_end_time + time_window_2;
+
+                Long64_t T_tot = num_omicron_triggers*time_window; //total time analyzed
+
+                mu = (time_window*num_main_channel_triggers*num_omicron_triggers)/T_tot;
+
+                if( num_coinc_triggers > 0 ) {
+                      //  sig = gamma_p(mu, num_coinc_triggers);
+                        sigold = -num_coinc_triggers*log10(mu) + mu*log10(exp(1)) + lgammal(num_coinc_triggers+1)/log(10);
+                        //return sigold;
+                        std::cerr << "sig = " << sig << ". If close enough to infinity, print out sigold instead, which yields " << sigold << ". \n" << std::endl;
+                }
+
+                else {
+                        sig = 0;
+                        std::cerr << "No coincident triggers, and therefore no significance. Something is wrong." << std::endl;
+                }
+		return sigold;
 }
